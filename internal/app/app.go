2c6b0d7fb8fe55fee09f0df0f600eecf3e2b65cb
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2020-04-25 12:07:01 -0700

    Added some flags

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,14 @@
 package app
 
 import (
-	"time"
-
 	"github.com/urfave/cli"
+	"time"
 )
 
 // NewApp returns pointer to a new cli.App.
 func NewApp() *cli.App {
 	return &cli.App{
-		Name: "Sieve",
+		Name:    "Sieve",
 		Version: "0.0.1",
 		Authors: []*Author{
 			&cli.Author{
@@ -17,9 +16,39 @@ func NewApp() *cli.App {
 				Email: "parker.d.gabel@gmail.com",
 			},
 		},
-		Compiled: time.Now(),
-		Usage: "Sieve recursively searchs the current directory or path argument for matchs to a regex pattern.",
-		Flags: []Flag,
-		
+		Compiled:    time.Now(),
+		Description: "Sieve recursively searchs the current directory or path argument for matchs to a regex pattern.",
+		Flags: []Flag{
+			&cli.BoolFlag{
+				Name:  "ignore-case, i",
+				Usage: "Perform a case-insensitive search",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name:  "invert-match, v",
+				Usage: "Invert matchings. Prints lines that do not match the patterns",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name:  "count, c",
+				Usage: "Shows the number of lines that match the given patterns. Suppresses the normal output",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name:  "paths, P",
+				Usage: "Shows the path of all files that contain matchs. Suppresses the normal output",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "line-nums,  l",
+				Usage: "Prints the line numbers with the matchs.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "unordered, u",
+				Usage: "Sieve is a concurrent program. The matchs may come in any order. This flag prints the  matchs in the order they are recieved.",
+				Value: false,
+			},
+		},
 	}
 }
