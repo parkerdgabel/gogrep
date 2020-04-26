a82c775203b6f30bf07b950cb82b3a4b8c57eb2b
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2020-04-25 19:07:47 -0700

    Added all the flags

--- internal/app/app.go
+++ internal/app/app.go
@@ -35,20 +35,72 @@ func NewApp() *cli.App {
 				Value: false,
 			},
 			&cli.BoolFlag{
-				Name:  "paths, P",
-				Usage: "Shows the path of all files that contain matchs. Suppresses the normal output",
-				Value: false,
-			},
-			&cli.BoolFlag{
-				Name: "line-nums,  l",
+				Name:  "line-nums,  n",
 				Usage: "Prints the line numbers with the matchs.",
 				Value: false,
 			},
 			&cli.BoolFlag{
-				Name: "unordered, u",
-				Usage: "Sieve is a concurrent program. The matchs may come in any order. This flag prints the  matchs in the order they are recieved.",
+				Name:  "unordered, u",
+				Usage: "Sieve is a concurrent program. The matchs may come in any order. This flag prints the matchs in the order they are recieved.",
 				Value: false,
 			},
+			&cli.BoolFlag{
+				Name:  "gitignore, g",
+				Usage: "Seach all files matched in a gitignore file. By default, sieve will not search files matched by the gitignore file.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "no-color",
+				Usage: "Disables colorized output.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "no-recurse",
+				Usage: "Disables recursive searching for the pattern. Sieve will just search the files in the given directory or the working directory if none is given.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "binary",
+				Usage: "Searches files that appear to be  binaries.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "list-files-with-match, l",
+				Usage: "Lists all the files that contain a match.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "list-files-without-match, L",
+				Usage: "Lists all the files that do not contain a match",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "quiet, q",
+				Usage: "Suppresses normal output. Returns with 0 exit code if a match was found.",
+				Value: false,
+			},
+			&cli.BoolFlag{
+				Name: "stats",
+				Usage: "Prints the statistics for the search at the end of the output.",
+				Value: false,
+			},
+			&cli.StringFlag{
+				Name:  "regex, e",
+				Usage: "The pattern the search will match.",
+			},
+			&cli.StringFlag{
+				Name: "output, o",
+				Usage: "Specifies the output destination, either a FILE or a network connection.",
+			},
+			&cli.StringSliceFlag{
+				Name: "files, f",
+				Usage: "Search all files matching `GLOB`.",
+			},
+			&cli.StringFlag{
+				Name: "exclude-files",
+				Usage: "Excludes all files matching `GLOB` from the search.",
+			},
+			
 		},
 	}
 }
